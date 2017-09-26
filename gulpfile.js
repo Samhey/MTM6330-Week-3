13a8f3e7da3bc2c8fda1a1d35465fa8df5666cfc
Samantha MacNeil <macn0112@algonquinlive.com>
2017-09-26 15:55:39 -0400

    Add Sass and compile to CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 
 gulp.task('sass', function () {
-  return gulp 
+  return gulp
      .src('scss/**/*.scss')
      .pipe(sass())
      .pipe(gulp.dest('css'))
